b922649f0c0d97bec355f484684db9823705872d
tung1099 <dtroyal1102@gmail.com>
2022-06-07 23:47:13 +0700

    CRUD Category basically done

--- src/app/category/category-edit/category-edit.component.ts
+++ src/app/category/category-edit/category-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {CategoryService} from '../../service/category/category.service';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-category-edit',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryEditComponent implements OnInit {
 
-  constructor() { }
+  categoryForm: FormGroup;
+  id: number;
+  constructor(private categoryService: CategoryService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.id = +paramMap.get('id');
+      this.getCategory(this.id);
+    });
+  }
 
   ngOnInit() {
   }
 
+   getCategory(id: number) {
+    return this.categoryService.findCategoryById(id).subscribe(category => {
+      this.categoryForm = new FormGroup({
+        name: new FormControl(category.name),
+      });
+    });
+  }
+  updateCategory(id: number) {
+    const category = this.categoryForm.value;
+    this.categoryService.updateCategory(id, category).subscribe(() => {
+      Swal.fire({
+        position: 'top-left',
+        icon: 'success',
+        title: 'Chỉnh sửa thành công',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.router.navigate(['category']);
+    });
+  }
 }
