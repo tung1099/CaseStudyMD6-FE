b256f9d24d0878733c0afeb6b5a70fac4bfe24f7
tung1099 <dtroyal1102@gmail.com>
2022-06-07 20:23:40 +0700

    basically done

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -6,13 +6,15 @@ import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryCreateComponent } from './category-create/category-create.component';
 import { CategoryEditComponent } from './category-edit/category-edit.component';
 import { CategoryDeleteComponent } from './category-delete/category-delete.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryCreateComponent, CategoryEditComponent, CategoryDeleteComponent],
-  imports: [
-    CommonModule,
-    CategoryRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        CategoryRoutingModule,
+        ReactiveFormsModule
+    ]
 })
 export class CategoryModule { }
