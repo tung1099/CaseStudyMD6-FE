4a61f079859cdba35883a4511874861acbedc7a8
CanhNguyen <leagueyasuo186@gmail.com>
2022-06-09 13:58:13 +0700

    transaction

--- src/app/transaction/transaction-delete/transaction-delete.component.ts
+++ src/app/transaction/transaction-delete/transaction-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import {TransactionService} from '../../service/transaction/transaction.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-transaction-delete',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionDeleteComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private transactionService: TransactionService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.activatedRoute.paramMap.subscribe((paramMap) => {
+      const id = paramMap.get('id');
+      this.delete(id);
+    });
+  }
 
   ngOnInit() {
+  }
+
+  delete(id) {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    });
+
+    swalWithBootstrapButtons.fire({
+      title: 'Are you sure?',
+      text: 'You won\'t be able to revert this!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, cancel!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.transactionService.delete(id).subscribe(() => {
+          this.router.navigate(['/transaction/listTransaction']);
+        });
+        swalWithBootstrapButtons.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        );
+      } else if (
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        this.router.navigate(['/transaction/listTransaction']);
+        swalWithBootstrapButtons.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        );
+      }
+    });
   }
 
 }
