4e4ec1e7513f3b93c42c5528150326e0ece952f9
NguyenAnh1212 <96817453+NguyenAnh1212@users.noreply.github.com>
2022-06-11 17:32:25 +0700

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,10 +23,6 @@ const routes: Routes = [
     loadChildren: () => import('./wallet/wallet.module').then(module => module.WalletModule)
   },
   {
-    path: 'type',
-    loadChildren: () => import('./moneytype/moneytype.module').then(module => module.MoneytypeModule)
-  },
-  {
     path: 'icon',
     loadChildren: () => import('./icon/icon.module').then(module => module.IconModule)
   },
