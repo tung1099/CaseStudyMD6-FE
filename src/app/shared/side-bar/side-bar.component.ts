372631303752e575901bec90d776f255662b86ed
cuongng22 <theendvg2003@gmail.com>
2022-06-13 05:31:14 +0700

    add

--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthencicationService} from '../../service/auth/authencication.service';
+import {UserInfo} from "../../model/user-info";
+import {UserInfoService} from "../../service/userInfo/user-info.service";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-side-bar',
@@ -7,10 +10,23 @@ import {AuthencicationService} from '../../service/auth/authencication.service';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
-idUser: number;
-  constructor(private authService: AuthencicationService) {}
+idUser: number = 0;
+userInfo: UserInfo = {};
+  constructor(private authService: AuthencicationService,
+              private userInfoService: UserInfoService
+              ) {
+    if (this.authService.currentUserValue!= null) {
+      this.idUser = this.authService.currentUserValue.id;
+    }
+    this.findByUserId(this.idUser)
+  }
   ngOnInit() {
-    this.idUser = this.authService.currentUserValue.id;
+
+  }
+  findByUserId(id) {
+    this.userInfoService.findByUserId(id).subscribe(profile => {
+      this.userInfo = profile;
+    })
   }
 
 }
