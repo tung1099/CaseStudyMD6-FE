6e5f520b8a11e9ab3b52e9bb10e4f6e607e0c779
tung1099 <dtroyal1102@gmail.com>
2022-06-13 14:07:00 +0700

    add

--- src/app/model/transaction.ts
+++ src/app/model/transaction.ts
@@ -1,8 +1,10 @@
+import {Wallet} from './wallet';
+
 export interface Transaction {
   id?: number;
   amount?: string;
   date?: string;
   note?: string;
   category?: any;
-  wallet?: any;
+  wallet?: Wallet;
 }
