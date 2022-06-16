c45993a5b51a0dc5d118da546d0b585e63ca5ce6
NguyenAnh1212 <96817453+NguyenAnh1212@users.noreply.github.com>
2022-06-16 17:34:05 +0700

    add

--- src/app/wallet/wallet-list/wallet-list.component.ts
+++ src/app/wallet/wallet-list/wallet-list.component.ts
@@ -8,6 +8,8 @@ import {SumMoney} from '../../model/sum-money';
 // import Swal from "sweetalert2";
 // import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {ShareWalletService} from '../../service/shareWallet/share-wallet.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-wallet-list',
@@ -15,16 +17,6 @@ import {ShareWalletService} from '../../service/shareWallet/share-wallet.service
   styleUrls: ['./wallet-list.component.css']
 })
 export class WalletListComponent implements OnInit {
-  // select = "Chọn ví";
-  // userId: number = 0;
-  // checkMail = false;
-  // checkUser: boolean;
-  // username: string;
-  // submitted = false;
-  wallets: Wallet[] = [];
-  wallet: Wallet = {};
-  idUser = 0;
-  sumMoneys: SumMoney[] = [];
 
 
 
@@ -37,15 +29,25 @@ export class WalletListComponent implements OnInit {
     }
     console.log(this.idUser);
   }
+  select = 'Chọn ví';
+  userId: number;
+  checkMail = false;
+  checkUser: boolean;
+  username: string;
+  submitted = false;
+  wallets: Wallet[] = [];
+  wallet: Wallet = {};
+  idUser = 0;
+  sumMoneys: SumMoney[] = [];
+  shareForm: FormGroup = new FormGroup({
+    wallet: new FormControl('', Validators.required),
+    user: new FormControl('', Validators.required)
+  });
 
   ngOnInit() {
     this.getAllWalletByUser();
     this.getSumMoney();
   }
-  // shareForm: FormGroup = new FormGroup({
-  //   wallet: new FormControl('', Validators.required),
-  //   user: new FormControl('', Validators.required)
-  // })
   getAllWalletByUser() {
     this.walletService.getAllByUserId(this.idUser).subscribe((wallets) => {
       this.wallets = wallets;
@@ -58,41 +60,41 @@ export class WalletListComponent implements OnInit {
       console.log(sumMoneys);
     });
   }
-  // share() {
-  //   this.submitted = true;
-  //   if (this.shareForm.valid) {
-  //     this.shareService.addNewShare(this.shareForm.get('wallet').value, this.shareForm.get('user').value).subscribe(() => {
-  //       this.shareForm.reset()
-  //       Swal.fire({
-  //         position: 'top-end',
-  //         icon: 'success',
-  //         title: 'Gửi thành công!',
-  //         showConfirmButton: false,
-  //         timer: 1500});
-  //     },err => {
-  //       Swal.fire({
-  //         position: 'top-end',
-  //         icon: 'error',
-  //         title: 'Ví này dã được chia sẻ cho người dùng!',
-  //         showConfirmButton: false,
-  //         timer: 1500});
-  //     })
-  //   } else {
-  //     // @ts-ignore
-  //     Swal.fire({
-  //       position: 'top-end',
-  //       icon: 'error',
-  //       title: 'Vui lòng nhập đúng định dạng!',
-  //       showConfirmButton: false,
-  //       timer: 1500});
-  //   }
-  // }
+  share() {
+    this.submitted = true;
+    if (this.shareForm.valid) {
+      this.shareService.addNewShare(this.shareForm.get('wallet').value, this.shareForm.get('user').value).subscribe(() => {
+        this.shareForm.reset();
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Gửi thành công!',
+          showConfirmButton: false,
+          timer: 1500});
+      },error => {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          title: 'Ví này dã được chia sẻ cho người dùng!',
+          showConfirmButton: false,
+          timer: 1500});
+      });
+    } else {
+      // @ts-ignore
+      Swal.fire({
+        position: 'top-end',
+        icon: 'error',
+        title: 'Vui lòng nhập đúng định dạng!',
+        showConfirmButton: false,
+        timer: 1500});
+    }
+  }
 
-  // usernameCheck($event) {
-  //   this.username = $event.target.value;
-  //   this.authService.checkUserName(this.username).subscribe((check) => {
-  //     this.checkUser = check;
-  //   });
-  // }
+  usernameCheck($event) {
+    this.username = $event.target.value;
+    this.authService.checkUserName(this.username).subscribe((check) => {
+      this.checkUser = check;
+    });
+  }
 
 }
